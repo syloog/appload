4bc0471d19a20eeb69b4256d51f64b94d66ebc48
akinparkan <47825161+akinparkan@users.noreply.github.com>
2019-12-30 00:26:52 +0300

    user can see their downloaded apps

---
+++ downloadedApps.php
@@ -0,0 +1,181 @@
+<?php
+include("session.php");
+if ($_SESSION["u_type"] != "regular") {
+    header("location: index.php");
+}
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Developer's apps</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=ABeeZee">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Titillium+Web:400,600,700">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="assets/css/Button-Change-Text-on-Hover.css">
+    <link rel="stylesheet" href="assets/css/Drag--Drop-Upload-Form.css">
+    <link rel="stylesheet" href="assets/css/ebs-contact-form.css">
+    <link rel="stylesheet" href="assets/css/Features-Blue.css">
+    <link rel="stylesheet" href="assets/css/Forum---Thread-listing.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/css/pikaday.min.css">
+    <link rel="stylesheet" href="assets/css/Lista-Productos-Canito.css">
+    <link rel="stylesheet" href="assets/css/Google-Style-Login.css">
+    <link rel="stylesheet" href="assets/css/Pretty-Registration-Form.css">
+    <link rel="stylesheet" href="assets/css/Pretty-Search-Form.css">
+    <link rel="stylesheet" href="assets/css/Profile-Edit-Form.css">
+    <link rel="stylesheet" href="assets/css/Registration-Form-with-Photo.css">
+    <link rel="stylesheet" href="assets/css/Search-Field-With-Icon.css">
+    <link rel="stylesheet" href="assets/css/untitled.css">
+    <link rel="stylesheet" href="assets/css/User-Information-Panel---Lite--Secondary-User-Panel-Footer.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-white portfolio-navbar gradient" style="font-family: Roboto, sans-serif;opacity: 1;background-image: url(&quot;assets/img/Rectangle%201.png&quot;);">
+        <div class="container"><a class="navbar-brand logo" href="home.html">AppLoad</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navbarNav"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item" role="presentation"><a class="nav-link" <?php
+                                                                                    if (isset($_SESSION["loggedin"])) {
+                                                                                        if ($_SESSION["u_type"] == "regular") {
+                                                                                            echo "href='profileUser.php'";
+                                                                                        } else if ($_SESSION["u_type"] == "developer") {
+                                                                                            echo "href='profileDev.php'";
+                                                                                        } else if ($_SESSION["u_type"] == "editor") {
+                                                                                            echo "href='profileEditor.php'";
+                                                                                        }
+                                                                                    ?>> My Profile</a></li>
+                <?php
+                                                                                    } else {
+                                                                                        echo "></a></li>";
+                                                                                    }
+                ?>
+                <li class="nav-item" role="presentation"><a class="nav-link" <?php
+                                                                                if (isset($_SESSION["loggedin"])) {
+                                                                                ?> href='forum.php?sort=lastest&pageno=1'>Forum</a></li>
+            <?php
+                                                                                } else {
+                                                                                    echo "></a></li>";
+                                                                                }
+            ?>
+            <li class="nav-item" role="presentation"><a class="nav-link" <?php
+                                                                            if (isset($_SESSION["loggedin"])) {
+                                                                            ?> href='store.php'>Store</a></li>
+        <?php
+                                                                            } else {
+                                                                                echo "></a></li>";
+                                                                            }
+        ?>
+        <li class="nav-item" role="presentation"><a class="nav-link" <?php
+                                                                        if (isset($_SESSION["loggedin"])) {
+                                                                        ?> href='logout.php'>Logout</a></li>
+    <?php
+                                                                        } else {
+                                                                            echo "href='loginScreen.php'>Login</a></li>";
+                                                                        }
+    ?>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <main class="page projects-page">
+        <section class="portfolio-block projects-with-sidebar">
+            <div class="container">
+                <div class="heading">
+                    <h2>MY APPS</h2>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-md-9">
+                        <?php
+                        if (isset($_SESSION["error"])) {
+                            foreach ($_SESSION["error"] as $error) {
+                                echo "<div class='row'><div class='col d-xl-flex justify-content-xl-center'><div class='alert alert-danger' role='alert' style='width:100%'><span><strong> Alert </strong>";
+                                echo $error;
+                                echo " </span></div></div></div>";
+                            }
+                            unset($_SESSION["error"]);
+                        } else if (isset($_SESSION["success"])) {
+                            foreach ($_SESSION["success"] as $success) {
+                                echo "<div class='row'><div class='col d-xl-flex justify-content-xl-center'><div class='alert alert-success' role='alert' style='width:100%;padding-left: 35px'><span><strong> Success: </strong>";
+                                echo $success;
+                                echo " </span></div></div></div>";
+                            }
+                            unset($_SESSION["success"]);
+                        }
+                        ?>
+                        <div class="row">
+                            <?php
+                            $dev_id = $_SESSION["u_id"];
+                            $query = 'Select * from application
+                                      where app_id IN (SELECT app_id
+                                                       From downloads where dev_id = ' . $dev_id . ')';
+                            $result = mysqli_query($db, $query);
+                            $i = 0;
+                            while ($row = mysqli_fetch_array($result)) {
+                                if ($i == 0) {
+                                    echo '<div class="row">';
+                                }
+                                $i++;
+                                echo '<div class="col align-self-center project-sidebar-card">
+                        <a href="appPage.php?appname=' . $row["appname"] . '">';
+                                echo '<div>';
+                                echo '<img class="img-fluid image scale-on-hover" src=./images/application_photos/' . $row["appLogo"] . ' name= ' . $row["appname"] . '></div>
+                        </a>
+                        <div>
+                        <p class="text-center border rounded-0" style="background-color: #e0e0e0;"><strong>App Name : </strong>' . $row["appname"] . '</p>
+                        <p class="text-center border rounded-0" style="background-color: ';
+                                if ($row["app_status"] == "WAITING") {
+                                    echo '#e0e0e0';
+                                } else if ($row["app_status"] == "APPROVED") {
+                                    echo '#acf74c';
+                                }
+                                echo '"><strong>Status : </strong>' . $row["app_status"] . '</p>
+                        </div>
+                        </div>';
+                                if ($i == 5) {
+                                    $i = 0;
+                                    echo '</div>';
+                                }
+                            }
+                            ?>
+                        </div>
+                        <nav class="col d-xl-flex justify-content-xl-center">
+                            <ul class="pagination">
+                                <li class="page-item"><a class="page-link" href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#">4</a></li>
+                                <li class="page-item"><a class="page-link" href="#">5</a></li>
+                                <li class="page-item"><a class="page-link" href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer class="page-footer">
+        <div class="container">
+            <div class="links"><a href="#">About us</a><a href="contact.html">Contact us</a><a href="store.html">Store</a></div>
+            <div class="social-icons"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-instagram-outline"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a></div>
+        </div>
+    </footer>
+    <div class="col"></div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/pikaday.min.js"></script>
+    <script src="assets/js/Profile-Edit-Form.js"></script>
+    <script src="assets/js/theme.js"></script>
+</body>
+
+</html>
