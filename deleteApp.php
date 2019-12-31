51b4460dcf6909f49659e993624ae472fcb8b17c
Can Gülsever <selimgulsever@gmail.com>
2019-12-31 17:42:16 +0300

    App Update/Delete feature is added.

---
+++ deleteApp.php
@@ -0,0 +1,20 @@
+<?php
+    include('session.php');
+    $var = $_GET["app_id"];
+    $query = "delete from application where app_id= ".$var;
+    if(mysqli_query($db,$query))
+    {
+        array_push($success, "Application is deleted.");
+        $_SESSION["success"] = $success;
+        header("location: devApps.php");
+    }
+    else
+    {
+           array_push($errors, "Cannot delete the app.");
+           $_SESSION["error"] = $errors;
+           header("location: manageApplication.php");
+    }
+?>
+
+
+  
